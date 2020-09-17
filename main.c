30ac024e373e278d867c8a32ec11d9499b7322ee
Jesus Alejandro <jesus.garcia1953@alumnos.udg.com.mx>
2020-09-16 20:56:30 -0500

    Capturamos el nombre e imprimimos un saludo

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    
+    char nombre[100];
+
     printf("Hola mundo\n");
+    
+    printf("Nombre: ");
+    scanf("%s",nombre);
+    printf("Hola %s\n", nombre);
+
     return 0;
 }
